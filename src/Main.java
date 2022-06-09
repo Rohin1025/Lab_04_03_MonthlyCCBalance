3e4af6900d3d1966b942fedf9b3e440f23c55bd6
Rohin <kotianra@mail.uc.edu>
2022-06-09 12:00:41 -0400

    Share init

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
-public class
-Main
-{
+public class Main {
     public static void main(String[] args)
     {
-        int number = 3; // random number used here to depict odd/even
-        int DIVIDEND = 2;
-        int remainder = number % DIVIDEND;
-        if (remainder > 0)
-        {
-            System.out.println(number + " is odd");
-        }
-        else
-        {
-            System.out.println(number + " is even");
-        }
+        double initialBalance = 5000.00;
+        double INTEREST_RATE = 1.17;
+        double oneMonthBalance = 0;
+        double twoMonthBalance = 0;
+        oneMonthBalance = initialBalance * INTEREST_RATE;
+        twoMonthBalance = oneMonthBalance * INTEREST_RATE;
+        System.out.println("Your initial balance is " + initialBalance);
+        System.out.println("Your balance after one month is " + oneMonthBalance);
+        System.out.println("Your balance after two months is " + twoMonthBalance);
     }
 }
